b6af7d53d6c26bb443edcb9f035e2185c1c34640
朱祖坤 <19373529@buaa.edu.cn>
2021-11-08 22:24:36 +0800

    kyaruk:bottom line

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,11 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:buaacourse/constants.dart';
 
+int _selectedIndex = 0;
+// 创建数组引入页面
+List pglist = [];
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: kTextColor,
+        type: BottomNavigationBarType.fixed,
+        elevation: 5.0,
+        unselectedFontSize: 12.0,
+        selectedFontSize: 18.0,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Find')),
+          BottomNavigationBarItem(icon: Icon(null), title: Text('Cart')),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_filter), title: Text('Zone')),
+          BottomNavigationBarItem(icon: Icon(Icons.face), title: Text('Ucenter')),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: _selectedIndex == 2 ? Colors.red : Colors.grey,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.add)
+            ]
+        ),
+        onPressed: (){
+          _selectedIndex = 2;
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
@@ -38,5 +71,9 @@ class HomeScreen extends StatelessWidget {
         SizedBox(width: kDefaultPaddin / 2)
       ],
     );
+  }
+
+  void _onItemTapped(int index) {
+    _selectedIndex = index;
   }
 }
