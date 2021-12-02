a15e497cf5e24f989895606c1e696b4dd4f42fe0
starlight <19373533@buaa.edu.cn>
2021-12-02 14:27:22 +0800

    change queryCourse URL

--- lib/screens/course/http_service.dart
+++ lib/screens/course/http_service.dart
@@ -6,17 +6,17 @@ import 'dart:convert';
 class Httpservice {
   final String postsUrl = "http://127.0.0.1:5000/";
 
-  Future<List<Course>> getCourses() async{
+  Future<List<Course>> getCourses() async {
     Response response = await get(Uri.parse(postsUrl));
 
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       List<dynamic> body = jsonDecode(response.body);
-      
-      List<Course> courses = body.map((dynamic item) => Course.fromJson(item)).toList();
+
+      List<Course> courses =
+          body.map((dynamic item) => Course.fromJson(item)).toList();
 
       return courses;
-    }
-    else {
+    } else {
       throw "Can't get posts.";
     }
   }
